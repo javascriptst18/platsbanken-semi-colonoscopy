db5c1b3d91ef5c2c8231d74f6946185556192a07
Felix H W <felixhow@icloud.com>
2018-06-26 13:34:19 +0200

    Add developer branch

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,10 @@ async function searchByCriteria(searchCriteria) {
   const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
   const responseObject = await fetch(baseURL + searchCriteria);
   const matches = await responseObject.json();
-  console.log(matches);
+  const matchesData = matches.matchningslista.matchningdata;
+  for (let each of matchesData)
+  console.log(each.annonsrubrik);
+
 }
 
 searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
