b026e135f80dc78b856a9096a44d7414f8c23c4e
Felix H W <felixhow@icloud.com>
2018-06-27 00:48:11 +0200

    Add keyword search functionality to city search

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,9 @@ let apiData = "";
 let divToPrint = "";
 
 // form för att söka på stad
-const citySearch = document.querySelector("#citySearch");
+const mainSearchForm = document.querySelector("#mainSearchForm");
+// input field för att söka på nyckelord
+const searchKeyword = document.querySelector("#searchKeyword");
 // checkbox för att välja stockholm
 const cityStockholm = document.querySelector("#cityStockholm");
 // checkbox för att välja göteborg
@@ -19,6 +21,8 @@ let stockholm = "";
 let goteborg = "";
 // tom variabel för att lagra malmö söksträng
 let malmo = "";
+// tom variabel för att lagra nyckelord i sökning
+let nyckelord = "";
 
 
 async function searchByCriteria(searchCriteria) {
@@ -60,11 +64,15 @@ let printTop10 = function(apiData){
   cardWrapper.innerHTML = divToPrint; 
 }
 
-
-
 // lyssna på submit från form för sökning på stad
-citySearch.addEventListener("submit", (event) => {
+mainSearchForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  // kolla om input field har innehåll och lagra i variabeln
+  if (searchKeyword.value !== "") {
+    nyckelord = searchKeyword.value;
+  } else {
+    nyckelord = "";
+  }
   // kolla om stockholm-boxen är checkad och lagra i variabeln
   if (cityStockholm.checked) {
     stockholm = "&lanid=1";
@@ -83,21 +91,17 @@ citySearch.addEventListener("submit", (event) => {
   } else {
     malmo = "";
   }
-  searchByCriteria(`platsannonser/matchning?${stockholm}${goteborg}${malmo}&yrkesomradeid=3&antalrader=30`);
+  searchByCriteria(`platsannonser/matchning?nyckelord=${nyckelord}${stockholm}${goteborg}${malmo}&antalrader=30`);
+  // searchByCriteria(`platsannonser/matchning?${stockholm}${goteborg}${malmo}&nyckelord=${searchKeyword}&antalrader=30`);
 });
 
-
-
-
-
-async function fetchData(url){
-  try{
-      let result = await fetch(url);
-      let resultResolve = await result.json();
-      return resultResolve; 
+async function fetchData(url) {
+  try {
+    let result = await fetch(url);
+    let resultResolve = await result.json();
+    return resultResolve; 
   } catch(error) {
       return error;
   }
- 
 }
   
