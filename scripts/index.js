250e4cb98d2e59feeb9afccc3d56331d2a1fdb8a
Felix H W <felixhow@icloud.com>
2018-06-26 15:31:35 +0200

    Add code for checkbox search results

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,45 @@
+// form för att söka på stad
+const citySearch = document.querySelector("#citySearch");
+// checkbox för att välja stockholm
+const cityStockholm = document.querySelector("#cityStockholm");
+// checkbox för att välja göteborg
+const cityGoteborg = document.querySelector("#cityGoteborg");
+// checkbox för att välja malmö
+const cityMalmo = document.querySelector("#cityMalmo");
+
+// tom variabel för att lagra stockholm söksträng
+let stockholm = "";
+// tom variabel för att lagra göteborg söksträng
+let goteborg = "";
+// tom variabel för att lagra malmö söksträng
+let malmo = "";
+
 async function searchByCriteria(searchCriteria) {
   const baseURL = 'http://api.arbetsformedlingen.se/af/v0/';
   const responseObject = await fetch(baseURL + searchCriteria);
   const matches = await responseObject.json();
-  const matchesData = matches.matchningslista.matchningdata;
-  for (let each of matchesData)
-  console.log(each.annonsrubrik);
-
 }
 
-searchByCriteria('platsannonser/matchning?lanid=1&yrkesomradeid=3&antalrader=30');
+// lyssna på submit från form för sökning på stad
+citySearch.addEventListener("submit", (event) => {
+  event.preventDefault();
+  // kolla om stockholm-boxen är checkad och lagra i variabeln
+  if (cityStockholm.checked) {
+    stockholm = "&lanid=1";
+  } else {
+    stockholm = "";
+  }
+  // kolla om göteborg-boxen är checkad och lagra i variabeln
+  if (cityGoteborg.checked) {
+    goteborg = "&lanid=14";
+  } else {
+    goteborg = "";
+  }
+  // kolla om malmö-boxen är checkad och lagra i variabeln
+  if (cityMalmo.checked) {
+    malmo = "&lanid=12";
+  } else {
+    malmo = "";
+  }
+  searchByCriteria(`platsannonser/matchning?${stockholm}${goteborg}${malmo}&yrkesomradeid=3&antalrader=30`);
+});
